136c39cfbdbb42c9877c2c84b60b5db73f9ab9b4
Andrew Stewart Gibson <andrew@goofballlogic.com>
2018-01-20 18:49:27 +0000

    user home

--- logic/action.js
+++ logic/action.js
@@ -12,3 +12,51 @@ var urlParams;
        urlParams[decode(match[1])] = decode(match[2]);
 })();
 
+window.attempt = function( logic ) {
+    
+    /* global localStorage */
+    try {
+        
+        logic();
+        
+    } catch( ex ) {
+        
+        var result = document.querySelector( "#result" ) || document.body;
+        result.innerHTML = "<h1>Oops</h1><p>That didn't work :(.</p>";
+        result.innerHTML += ex.message;
+        result.innerHTML += JSON.stringify( localStorage, null, 3 );
+        
+    }
+    
+};
+
+window.populate = function( selector, dataOrStrategy, maybeContext ) {
+
+    var maybeContext = maybeContext || document;
+    var strategy = typeof dataOrStrategy === "function" 
+        ? dataOrStrategy 
+        : function( element ) { element.textContent = dataOrStrategy; };
+    for( var element of maybeContext.querySelectorAll( selector ) )
+        strategy( element );
+        
+};
+
+window.entemplate = function( templateSelector, containerSelector, collection, populateStrategy  ) {
+  
+    var container = document.querySelector( containerSelector );
+    container.innerHTML = "";
+    var content = document.querySelector( templateSelector ).content;
+    collection.forEach( function( item ) {
+        
+        var node = document.importNode( content, true );
+        populateStrategy( 
+            
+            function( selector, dataOrStrategy ) { window.populate( selector, dataOrStrategy, node ); },
+            item
+            
+        );
+        container.appendChild( node );
+
+    } );
+    
+};
